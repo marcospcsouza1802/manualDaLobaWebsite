f7acd297e7ce767f0fcbaafda6666dd46f94e0fe
Marcos <marcospcsouza1802@gmail.com>
2023-06-17 10:51:45 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSubmit() {
+    document.getElementById('btnComprar').style.display = 'block';
+    document.getElementById('precoPromocional').style.display = 'block';
+    document.getElementById('ofertaLimitada').style.display = 'block';
+    document.getElementById('precoOriginal').style.display = 'block';
+}
+
+setTimeout(showSubmit, 495000);;
